086a938ac4e15ac15d1675005bc259fcc3812fb7
Jimmy Brisson <theotherjimmy@gmail.com>
2018-05-10 11:24:33 -0500

    Reformat base tests with Black

--- test/base.py
+++ test/base.py
@@ -17,14 +17,9 @@ limitations under the License.
 """
 
 import unittest
-import os
-import errno
-import logging
-import re
 import pkg_resources
 import json
 from mock import patch, MagicMock
-from copy import deepcopy
 from six import StringIO
 
 import mbed_lstools.main as cli
@@ -36,19 +31,24 @@ except NameError:
     # Python 3
     basestring = str
 
+
 class CLIComands(unittest.TestCase):
     """ Test the CLI
     """
 
     def setUp(self):
-        self._stdout = patch('sys.stdout', new_callable=StringIO)
+        self._stdout = patch("sys.stdout", new_callable=StringIO)
         self.stdout = self._stdout.start()
         self.mbeds = MagicMock()
         self.args = MagicMock()
         self.mbeds.list_mbeds.return_value = [
-            {'platform_name': 'foo', 'platform_name_unique': 'foo[0]',
-             'mount_point': 'a mount point', 'serial_port': 'a serial port',
-             'target_id': 'DEADBEEF', 'daplink_version': 'v12345'
+            {
+                "platform_name": "foo",
+                "platform_name_unique": "foo[0]",
+                "mount_point": "a mount point",
+                "serial_port": "a serial port",
+                "target_id": "DEADBEEF",
+                "daplink_version": "v12345",
             }
         ]
 
@@ -57,8 +57,9 @@ class CLIComands(unittest.TestCase):
 
     def test_print_version(self):
         cli.print_version(self.mbeds, self.args)
-        self.assertIn(pkg_resources.require('mbed-ls')[0].version,
-                      self.stdout.getvalue())
+        self.assertIn(
+            pkg_resources.require("mbed-ls")[0].version, self.stdout.getvalue()
+        )
 
     def test_print_table(self):
         cli.print_table(self.mbeds, self.args)
@@ -74,8 +75,9 @@ class CLIComands(unittest.TestCase):
 
     def test_mbeds_as_json(self):
         cli.mbeds_as_json(self.mbeds, self.args)
-        self.assertEqual(self.mbeds.list_mbeds.return_value,
-                         json.loads(self.stdout.getvalue()))
+        self.assertEqual(
+            self.mbeds.list_mbeds.return_value, json.loads(self.stdout.getvalue())
+        )
 
     def test_json_by_target_id(self):
         cli.json_by_target_id(self.mbeds, self.args)
@@ -85,27 +87,31 @@ class CLIComands(unittest.TestCase):
 
     def test_json_platforms(self):
         cli.json_platforms(self.mbeds, self.args)
-        platform_names = [d['platform_name'] for d
-                          in self.mbeds.list_mbeds.return_value]
+        platform_names = [
+            d["platform_name"] for d in self.mbeds.list_mbeds.return_value
+        ]
         for name in json.loads(self.stdout.getvalue()):
             self.assertIn(name, platform_names)
 
     def test_json_platforms_ext(self):
         cli.json_platforms_ext(self.mbeds, self.args)
-        platform_names = [d['platform_name'] for d
-                          in self.mbeds.list_mbeds.return_value]
+        platform_names = [
+            d["platform_name"] for d in self.mbeds.list_mbeds.return_value
+        ]
         for name in json.loads(self.stdout.getvalue()).keys():
             self.assertIn(name, platform_names)
 
     def test_list_platform(self):
-        self.mbeds.list_manufacture_ids.return_value ="""
+        self.mbeds.list_manufacture_ids.return_value = """
         foo
         bar
         baz
         """
         cli.list_platforms(self.mbeds, self.args)
-        self.assertIn(self.mbeds.list_manufacture_ids.return_value,
-                      self.stdout.getvalue())
+        self.assertIn(
+            self.mbeds.list_manufacture_ids.return_value, self.stdout.getvalue()
+        )
+
 
 class CLIParser(unittest.TestCase):
 
@@ -121,16 +127,17 @@ class CLIParser(unittest.TestCase):
 
     def test_parse_cli_conflict(self):
         try:
-            args = cli.parse_cli(["-j", "-J"])
             assert False, "Parsing conflicting options should have failed"
-        except:
+        except Exception:
             pass
 
     def test_parse_cli_single_param(self):
-        for p in ['j', 'J', 'p', 'P', '-version', 'd', 'u']:
-            args = cli.parse_cli(['-' + p])
+        for p in ["j", "J", "p", "P", "-version", "d", "u"]:
+            args = cli.parse_cli(["-" + p])
             assert callable(args.command)
 
+
 class CLISetup(unittest.TestCase):
+
     def test_start_logging(self):
         cli.start_logging()
