93ce32f2e3e96a0ba4a9dd4e61c6ccecfd581ad5
Jimmy Brisson <theotherjimmy@gmail.com>
2018-05-10 10:33:12 -0500

    Reformat setup.py with black

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 """
 This module defines the attributes of the
 PyPI package for the mbed SDK test suite ecosystem tools
-"""
 
-"""
 mbed SDK
 Copyright (c) 2011-2015 ARM Limited
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,41 +23,30 @@ from io import open
 from setuptools import find_packages
 
 DESCRIPTION = "mbed-ls is a Python module that detects and lists mbed-enabled devices connected to the host computer"
-OWNER_NAMES = 'Przemyslaw Wirkus, Johan Seferidis, James Crosby'
-OWNER_EMAILS = 'Przemyslaw.Wirkus@arm.com, Johan.Seferidis@arm.com, James.Crosby@arm.com'
+OWNER_NAMES = "Przemyslaw Wirkus, Johan Seferidis, James Crosby"
+OWNER_EMAILS = "Przemyslaw.Wirkus@arm.com, Johan.Seferidis@arm.com, James.Crosby@arm.com"
 
 
 # Utility function to cat in a file (used for the README)
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname), encoding="utf8").read()
 
-setup(name='mbed-ls',
-      version='1.4.4',
-      description=DESCRIPTION,
-      long_description=read('README.md'),
-      author=OWNER_NAMES,
-      author_email=OWNER_EMAILS,
-      maintainer=OWNER_NAMES,
-      maintainer_email=OWNER_EMAILS,
-      url='https://github.com/ARMmbed/mbed-ls',
-      packages=find_packages(),
-      license="Apache-2.0",
-      test_suite = 'test',
-      entry_points={
-        "console_scripts": [
-            "mbedls=mbed_lstools:mbedls_main",
-        ],
-      },
-      install_requires=[
-          "PrettyTable>=0.7.2",
-          "fasteners",
-          "appdirs>=1.4"
-      ],
-      tests_require = [
-          "mock>=2",
-          "pytest>=3"
-      ],
-      extras_require = {
-          "colorized_logs": ["colorlog"]
-      }
+
+setup(
+    name="mbed-ls",
+    version="1.4.4",
+    description=DESCRIPTION,
+    long_description=read("README.md"),
+    author=OWNER_NAMES,
+    author_email=OWNER_EMAILS,
+    maintainer=OWNER_NAMES,
+    maintainer_email=OWNER_EMAILS,
+    url="https://github.com/ARMmbed/mbed-ls",
+    packages=find_packages(),
+    license="Apache-2.0",
+    test_suite="test",
+    entry_points={"console_scripts": ["mbedls=mbed_lstools:mbedls_main"]},
+    install_requires=["PrettyTable>=0.7.2", "fasteners", "appdirs>=1.4"],
+    tests_require=["mock>=2", "pytest>=3"],
+    extras_require={"colorized_logs": ["colorlog"]},
 )
